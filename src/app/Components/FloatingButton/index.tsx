7db7ed0030ddd02527b0f70f7ae5d61b8446c6b8
Rodrigo Alves <igo.sud@hotmail.com>
2024-02-14 00:31:27 -0300

    ui add button

--- src/app/Components/FloatingButton/index.tsx
+++ src/app/Components/FloatingButton/index.tsx
@@ -10,7 +10,7 @@ const FloatingButton: React.FC<FloatingButtonProps> = ({ onClick }) => {
       onClick={onClick}
       className="fixed bottom-4 right-4 bg-blue-500 text-white px-4 py-2 rounded-full shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
     >
-      +
+      <strong>+</strong>
     </button>
   );
 };
