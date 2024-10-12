0f076ed8e03771b51fed646949cbd69ef38be99c
Rodrigo Alves <RodrigoAlves.Oliveira-ext@ab-inbev.com>
2024-10-12 11:05:30 -0300

    fetching data treatment updated

--- src/pages/api/translate.ts
+++ src/pages/api/translate.ts
@@ -1,14 +1,24 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+const ENDPOINT = "https://t.song.work/api";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { text } = req.query;
 
   try {
-    const response = await fetch(`https://t.song.work/api?text=${text}&from=en&to=pt`);
+    const response = await fetch(`${ENDPOINT}?text=${text}&from=en&to=pt`);
     if (!response.ok) {
-      throw new Error(`Error fetching translation: ${response.statusText}`);
+      res.status(400).json({ error: response.statusText });
     }
     const data = await response.json();
+
+    if (!data.result) {
+      data.result = data.translations.noun[0].translation;
+    }
+
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error });
